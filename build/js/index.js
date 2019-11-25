960f8c6fd8d0206c4d8ead3dc831fd71374b93d8
Гоша <losetown343@e1.ru>
2019-11-25 21:10:33 +0500

    Доверстал футер, сверстал модальное окно

--- build/js/index.js
+++ build/js/index.js
@@ -1,16 +1,40 @@
 "use strict";
-(() => {
-  let toggleSection = document.querySelectorAll(".info__title");
 
+let toggleSection = document.querySelectorAll(".info__title");
+
+if (toggleSection) {
   toggleSection.forEach(elem => elem.addEventListener("click", toggleHandler));
+}
 
-  function toggleHandler(e) {
-    e.preventDefault();
-    let section = e.target.parentNode.querySelector(".info-wrapper");
-    if (section.classList.contains("info-wrapper--hide")) {
-      section.classList.remove("info-wrapper--hide");
-    } else {
-      section.classList.add("info-wrapper--hide");
-    }
-  }
-})();
+function toggleHandler(e) {
+  e.preventDefault();
+  let section = e.target.parentNode.querySelector(".info-wrapper");
+
+  section.classList.toggle("info-wrapper--hide");
+}
+
+const writeUsBtn = document.querySelector(".js-write-us");
+
+if (writeUsBtn) {
+  writeUsBtn.addEventListener("click", openWriteUsModal);
+}
+
+function openWriteUsModal(e) {
+  e.preventDefault();
+  const modal = document.querySelector(".write-us-modal");
+  if (modal) modal.classList.remove("visually-hidden");
+}
+
+let modals = document.querySelectorAll(".modal");
+
+modals.forEach(elem => elem.addEventListener("click", closeModalHandler));
+
+function closeModalHandler(e) {
+  e.preventDefault();
+  const tg = e.target;
+  const closeButton = this.querySelector(".modal__close");
+  const overlay = this.querySelector(".modal__overlay");
+
+  if (tg === closeButton || tg === overlay)
+    this.classList.add("visually-hidden");
+}
--- source/js/index.js
+++ source/js/index.js
@@ -1,11 +1,40 @@
 "use strict";
 
-let toggleSection = document.querySelector(".info__title");
+let toggleSection = document.querySelectorAll(".info__title");
 
-toggleSection.addEventListener("click", toggleHandler);
+if (toggleSection) {
+  toggleSection.forEach(elem => elem.addEventListener("click", toggleHandler));
+}
 
 function toggleHandler(e) {
   e.preventDefault();
   let section = e.target.parentNode.querySelector(".info-wrapper");
-  section.classList.add("info-wrapper--hide");
+
+  section.classList.toggle("info-wrapper--hide");
+}
+
+const writeUsBtn = document.querySelector(".js-write-us");
+
+if (writeUsBtn) {
+  writeUsBtn.addEventListener("click", openWriteUsModal);
+}
+
+function openWriteUsModal(e) {
+  e.preventDefault();
+  const modal = document.querySelector(".write-us-modal");
+  if (modal) modal.classList.remove("visually-hidden");
+}
+
+let modals = document.querySelectorAll(".modal");
+
+modals.forEach(elem => elem.addEventListener("click", closeModalHandler));
+
+function closeModalHandler(e) {
+  e.preventDefault();
+  const tg = e.target;
+  const closeButton = this.querySelector(".modal__close");
+  const overlay = this.querySelector(".modal__overlay");
+
+  if (tg === closeButton || tg === overlay)
+    this.classList.add("visually-hidden");
 }
